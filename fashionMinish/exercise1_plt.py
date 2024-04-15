bbb699c59892cb063c850e70770b13d7ecc8dc10
Lu Wang <l236wang@gmail.com>
2024-04-15 00:31:04 -0400

    explore python pyplot lab

---
+++ fashionMinish/exercise1_plt.py
@@ -0,0 +1,38 @@
+"""Beyond Hello World, A Computer Vision Example"""
+
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+import cv2
+
+# setting values to rows and column variables
+rows = 4
+columns = 2
+
+labelDict = {
+    0: "T-shirt/top",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle boot",
+}
+
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+# create figure
+fig = plt.figure(figsize=(5, 7))
+
+for i in range(8):
+    fig.add_subplot(rows, columns, i + 1)
+    # showing image
+    plt.imshow(training_images[i])
+    plt.axis("off")
+    plt.title("{}:{}".format(training_labels[i], labelDict[training_labels[i]]))
+
+    # Adds a subplot at the 2nd position
---
+++ fashionMinish/fashionMnish.py
@@ -0,0 +1,51 @@
+"""Beyond Hello World, A Computer Vision Example"""
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+import tensorflow as tf
+from tensorflow import keras
+import matplotlib.pyplot as plt
+
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
+
+training_images  = training_images / 255.0
+test_images = test_images / 255.0
+
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(), 
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu), 
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
+
+model.compile(optimizer = tf.keras.optimizers.Adam(),
+              loss = 'sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.fit(training_images, training_labels, epochs=5)
+
+model.evaluate(test_images, test_labels)
+
+classifications = model.predict(test_images)
+
+
+# Label	Description
+# 0	T-shirt/top
+# 1	Trouser
+# 2	Pullover
+# 3	Dress
+# 4	Coat
+# 5	Sandal
+# 6	Shirt
+# 7	Sneaker
+# 8	Bag
+# 9	Ankle boot
+print(classifications[0])
+
+plt.figure()
+plt.imshow(training_images[data_idx], cmap='gray')
+plt.colorbar()
+plt.grid(False)
+plt.show()
